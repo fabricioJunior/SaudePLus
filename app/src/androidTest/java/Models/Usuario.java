3d81d4a4ba21049db8ff5f632fee056199feff8f
Fabricio Junior <fabriciojamescarneiro@gmail.com>
2020-05-28 16:13:36 -0300

    MVC

---
+++ app/src/androidTest/java/Models/Usuario.java
@@ -0,0 +1,77 @@
+package Models;
+
+public class Usuario {
+
+     private String nome;
+     private String cpf;
+     private String rg;
+     private String acesso;
+     private String senha;
+     private String telefone;
+     private String endereco;
+     private String email;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getAcesso() {
+        return acesso;
+    }
+
+    public void setAcesso(String acesso) {
+        this.acesso = acesso;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ app/src/androidTest/java/Utils/API.java
@@ -0,0 +1,7 @@
+package Utils;
+
+public class API {
+    private String url = "";
+    private String idToken = "";
+
+}
