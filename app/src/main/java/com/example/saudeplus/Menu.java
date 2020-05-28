ee464066347e7de848a278683e93c479570081c2
Fabricio Junior <fabriciojamescarneiro@gmail.com>
2020-05-28 15:14:23 -0300

    Initial commit

---
+++ app/src/main/java/com/example/saudeplus/Menu.java
@@ -0,0 +1,14 @@
+package com.example.saudeplus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
